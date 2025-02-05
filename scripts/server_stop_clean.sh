892d3d7d07c53452f96762759c8f6eb082921ce5
Bhavna <bhavna.rani199@gmail.com>
2025-02-05 17:41:59 +0530

    Update server_stop_clean.sh

--- scripts/server_stop_clean.sh
+++ scripts/server_stop_clean.sh
@@ -2,12 +2,4 @@
 
 # Stop Tomcat first
 sudo systemctl stop tomcat
-
-# Remove the old WAR file
-sudo rm -f /opt/tomcat/webapps/hello-world-maven.war
-
-# Remove the unpacked directory if it exists
-sudo rm -rf /opt/tomcat/webapps/hello-world-maven
-
-# Optionally clean up any temp files in the work directory (not strictly required)
-sudo rm -rf /opt/tomcat/work/*
+sudo rm -rf /opt/tomcat/webapps/hello-world-maven.war
