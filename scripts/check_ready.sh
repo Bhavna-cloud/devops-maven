874c1ac2c0ef629a1bc5e5f8557dda1cf22995f5
Bhavna <bhavna.rani199@gmail.com>
2025-03-03 16:16:53 +0530

    Update check_ready.sh

--- scripts/check_ready.sh
+++ scripts/check_ready.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the application is fully ready 
-while ! curl --silent --fail http://tomcat-alb-151555836.us-east-1.elb.amazonaws.com; do
+while ! curl --silent --fail http://tomcat-alb-151555836.us-east-1.elb.amazonaws.com/hello-world-maven; do
     echo "Waiting for the application to be ready..."
     sleep 5
 done
