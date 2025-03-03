51414103fda80442aea3ac6233365b37787a8588
Bhavna <bhavna.rani199@gmail.com>
2025-03-03 16:14:39 +0530

    Create check_ready.sh

---
+++ scripts/check_ready.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Check if the application is fully ready 
+while ! curl --silent --fail http://tomcat-alb-151555836.us-east-1.elb.amazonaws.com; do
+    echo "Waiting for the application to be ready..."
+    sleep 5
+done
+echo "Application is ready. Proceeding with traffic."
