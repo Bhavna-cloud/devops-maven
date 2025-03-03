8cd605ad029de8776113414f742004fa9c9200b9
Bhavna <bhavna.rani199@gmail.com>
2025-03-03 18:13:53 +0530

    Delete scripts/check_ready.sh

--- scripts/check_ready.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Check if the application is fully ready 
-while ! curl --silent --fail http://tomcat-alb-151555836.us-east-1.elb.amazonaws.com/hello-world-maven; do
-    echo "Waiting for the application to be ready..."
-    sleep 5
-done
-echo "Application is ready. Proceeding with traffic."
